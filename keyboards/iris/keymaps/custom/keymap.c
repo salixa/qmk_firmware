e3c835f640938c4fcbfc40a4205796ef4d33ad0b
asdf <koaalex@umich.edu>
2017-12-14 10:58:45 -0500

    Simplifying map
    Removing other layers for now and fixing R2

--- keyboards/iris/keymaps/custom/keymap.c
+++ keyboards/iris/keymaps/custom/keymap.c
@@ -5,27 +5,20 @@
 extern keymap_config_t keymap_config;
 
 #define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 16
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
+  QWERTY = SAFE_RANGE
 };
 
 #define KC_ KC_TRNS
 #define _______ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
   [_QWERTY] = KEYMAP(
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,         KC_6, KC_7, KC_8, KC_9, KC_0, KC_ESC,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     RAISE, KC_Q, KC_W, KC_E, KC_4, KC_5,         KC_Y, KC_U, KC_I, KC_O, KC_P, LOWER,
+     KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
      KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,          KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
@@ -33,56 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
                        KC_LCTL,KC_RALT,LSFT_T(KC_BSPC),        RSFT_T(KC_SPC), KC_RALT, KC_RCTL
   //                  `----+----+----'        `----+----+----'
-  ),
-
-  [_LOWER] = KEYMAP(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,               KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5,                KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     KC_DEL, KC_NO, KC_LEFT, KC_RGHT, KC_UP, KC_LBRC,               KC_RBRC, KC_P4, KC_P5, KC_P6, KC_PLUS, KC_PIPE,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     BL_STEP, KC_NO, KC_NO, KC_NO, KC_DOWN, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_P1, KC_P2, KC_P3, KC_MINS, KC_NO,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                           KC_NO, KC_NO, KC_DEL,         KC_DEL, KC_NO, KC_P0 
-  //                  `----+----+----'        `----+----+----'
-  ),
-
-  [_RAISE] = KEYMAP(
-  //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         KC_NO, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,               KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NO,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         KC_NO, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS,               KC_EQL, KC_HOME, KC_NO, KC_NO, KC_NO, KC_BSLS,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, KC_NO,      KC_NO, KC_PLUS,KC_END , KC_NO, KC_NO, KC_NO, KC_NO,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                           KC_NO, KC_NO, KC_NO,          KC_NO, KC_NO, KC_NO    
-  //                  `----+----+----'        `----+----+----'
-  ),
-
-  [_ADJUST] = KEYMAP(
-  //,--------+--------+--------+--------+--------+--------.                          ,--------+--------+--------+--------+--------+--------.
-      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      _______, DEBUG  , RGB_HUD, RGB_SAD, RGB_VAD, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------+--------.        ,--------|--------+--------+--------+--------+--------+--------|
-      BL_STEP, RESET  , _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
-  //`--------+--------+--------+----+---+--------+--------+--------/        \--------+--------+--------+---+----+--------+--------+--------'
-                                      _______, _______, _______,                  _______, _______, _______
-  //                                `--------+--------+--------'                `--------+--------+--------'
   )
-
 };
-
-#ifdef AUDIO_ENABLE
-float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
-#endif
-
 void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
@@ -92,38 +37,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_qwerty);
-        #endif
         persistent_default_layer_set(1UL<<_QWERTY);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
       }
       return false;
       break;
