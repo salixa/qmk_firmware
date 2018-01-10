24bfcf5d737214841ae2c5312b80e5afcc8295a3
asdf <koaalex@umich.edu>
2018-01-10 17:16:06 -0500

    Moving enter

--- keyboards/iris/keymaps/custom/keymap.c
+++ keyboards/iris/keymaps/custom/keymap.c
@@ -61,13 +61,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
      KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5,         KC_6, KC_7, KC_8, KC_9, KC_0, OSL(_LSWAP), //name?
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     OSL(_NUMPAD), KC_Q, KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO,
+     OSL(_NUMPAD), KC_Q, KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ENT,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
      KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,          KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_DEL,     KC_ENT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       LT(_NAV, KC_ENT), KC_BSPC, TD(MOD_DANCE),       TD(MOD_DANCE), KC_SPC, LT(_SYM, KC_ENT)
+                       OSL(_NAV), KC_BSPC, TD(MOD_DANCE),       TD(MOD_DANCE), KC_SPC, OSL(_SYM)
   //                  `----+----+----'        `----+----+----'
   ),
   
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
       KC_CAPS, _______, _______, KC_MINS, KC_UNDS, _______,                            _______, KC_EQL , KC_PLUS, _______, _______, KC_SLCK,
   //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      _______, _______, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT  ,                            KC_GT  , KC_RPRN, KC_RBRC, KC_LCBR, _______, KC_PIPE,
+      _______, _______, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT  ,                            KC_GT  , KC_RPRN, KC_RBRC, KC_RCBR, _______, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------+--------.        ,--------|--------+--------+--------+--------+--------+--------|
       _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,         KC_F7  ,  KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
   //`--------+--------+--------+----+---+--------+--------+--------/        \--------+--------+--------+---+----+--------+--------+--------'
