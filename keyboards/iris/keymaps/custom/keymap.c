cdcc69886145942b891526151a4f83257cd9680d
asdf <koaalex@umich.edu>
2018-01-24 17:56:57 -0500

    Cleanup and debugging
    Remove unused layers and compile flags
    Add colemak layer for fun
    Add keylogger

--- keyboards/iris/keymaps/custom/keymap.c
+++ keyboards/iris/keymaps/custom/keymap.c
@@ -5,18 +5,18 @@
 extern keymap_config_t keymap_config;
 
 #define _QWERTY 0
-#define _PATH 1
+#define _COLEMAK 1
 #define _SYM 2
-#define _NUMPAD 3
-#define _NAV 4
-#define _LSWAP 5
+#define _NAV 3
+#define _LSWAP 4
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  PATH,
+  COLEMAK,
   SYM,
   NAV,
-  LSWAP
+  LSWAP,
+  M_TOGGLE_LOGGING
 };
 
 enum tap_dance_actions {
@@ -26,42 +26,17 @@ enum tap_dance_actions {
 #define KC_ KC_TRNS
 #define _______ KC_TRNS
 
-
-// Temporary (?) defines for PoE bindings
-// Need: Skills x5, Potions x5, pickup, weapon, attack in place (how to do the numlock trick?), all UI elements
-#define SKILL1 KC_A
-#define SKILL2 KC_S
-#define SKILL3 KC_D
-#define SKILL4 KC_F
-#define SKILL5 KC_G
-#define POT1 KC_Q
-#define POT2 KC_W
-#define POT3 KC_E
-#define POT4 KC_R
-#define POT5 KC_T
-#define PICKUP KC_LSFT
-#define WSWAP KC_X
-#define MAP KC_TAB
-#define MINES KC_V
-#define HIGHLIGHT KC_Z
-#define ATTACK_IN_PLACE KC_F10
-#define INVENTORY KC_1
-#define STATS KC_2
-#define ATLAS KC_3
-#define SOCIAL KC_4
-#define PASSIVES KC_5
-#define OPTIONS KC_PIPE
-
 #define WORD_FW LCTL(KC_RIGHT)
 #define WORD_BW LCTL(KC_LEFT)
 
+bool log_enable = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP(
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5,         KC_6, KC_7, KC_8, KC_9, KC_0, OSL(_LSWAP), //name?
+     KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5,         KC_6, KC_7, KC_8, KC_9, KC_0, OSL(_LSWAP),
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     OSL(_NUMPAD), KC_Q, KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ENT,
+     KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ENT,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
      KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,          KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
@@ -71,17 +46,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                  `----+----+----'        `----+----+----'
   ),
   
-  [_PATH] = KEYMAP( 
+  [_COLEMAK] = KEYMAP( 
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     _______, INVENTORY, STATS, ATLAS, SOCIAL, PASSIVES,         KC_6, KC_7, KC_8, KC_9, KC_0, _______,
+     _______, KC_1, KC_2, KC_3, KC_4, KC_5,         KC_6, KC_7, KC_8, KC_9, KC_0, _______, 
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     _______, POT1, POT2, POT3, POT4, POT5,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LOCK,
+     _______, KC_Q, KC_W, KC_F, KC_P, KC_G,         KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_ENT,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     MAP, SKILL1, SKILL2, SKILL3, SKILL4, SKILL5,          KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+     KC_TAB, KC_A, KC_R, KC_S, KC_T, KC_D,          KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     OPTIONS, HIGHLIGHT, WSWAP, KC_C, MINES, KC_B, KC_ENT,     KC_ENT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, ATTACK_IN_PLACE,
+     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_DEL,     KC_ENT, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       PICKUP, _______, _______,        _______, _______, _______
+                       OSL(_NAV), KC_BSPC, TD(MOD_DANCE),       TD(MOD_DANCE), KC_SPC, OSL(_SYM)
   //                  `----+----+----'        `----+----+----'
   ),
   
@@ -89,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------+--------+--------+--------+--------+--------.                          ,--------+--------+--------+--------+--------+--------.
       _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      KC_CAPS, _______, _______, KC_MINS, KC_UNDS, _______,                            _______, KC_EQL , KC_PLUS, _______, _______, KC_SLCK,
+      KC_CAPS, _______, _______, KC_MINS, KC_UNDS, _______,                            _______, KC_EQL , KC_PLUS, KC_BSLS, _______, KC_SLCK,
   //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
       _______, _______, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT  ,                            KC_GT  , KC_RPRN, KC_RBRC, KC_RCBR, _______, KC_PIPE,
   //|--------+--------+--------+--------+--------+--------+--------.        ,--------|--------+--------+--------+--------+--------+--------|
@@ -97,20 +72,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //`--------+--------+--------+----+---+--------+--------+--------/        \--------+--------+--------+---+----+--------+--------+--------'
                                       _______, _______, OSM(KC_LGUI),        OSM(KC_RGUI), _______, _______
   //                                `--------+--------+--------'                `--------+--------+--------'
-  ),
-  
-  [_NUMPAD] = KEYMAP(
-    //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-      _______, KC_1, KC_2, KC_3, KC_4, KC_5,         KC_6, KC_7, KC_8, KC_9, KC_0, OSL(_LSWAP), //name?
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-      _______, _______, _______, _______, _______, _______,                             _______, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______,
-  //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-      _______, _______, _______, _______, _______, _______,                             _______, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______,
-  //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-      _______, _______, _______, _______, _______, _______, _______,           _______, KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______,
-  //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                                      _______, _______, _______,                  _______, _______, _______
-  //                  `----+----+----'        `----+----+----'
   ),
   
   [_NAV] = KEYMAP(
@@ -131,11 +92,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------+--------+--------+--------+--------+--------.                          ,--------+--------+--------+--------+--------+--------.
       TO(_QWERTY), _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSEL,                         _______, _______, _______, _______, _______, _______,
   //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, TO(_PATH), _______,
+      _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
   //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      _______, _______, TO(_SYM), _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+      _______, _______, TO(_SYM), _______, _______, _______,                            _______, _______, _______, M_TOGGLE_LOGGING, _______, _______,
   //|--------+--------+--------+--------+--------+--------+--------.        ,--------|--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, TO(_COLEMAK), _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
   //`--------+--------+--------+----+---+--------+--------+--------/        \--------+--------+--------+---+----+--------+--------+--------'
                                       _______, _______, _______,                  _______, _______, _______
   //                                `--------+--------+--------'                `--------+--------+--------'
@@ -148,10 +109,22 @@ void persistent_default_layer_set(uint16_t default_layer) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (log_enable && record->event.pressed) {
+    uint8_t layer = biton32(layer_state);
+    xprintf ("KL: col=%02d, row=%02d, pressed=%d, layer=%02d\n", record->event.key.col,
+            record->event.key.row, record->event.pressed, layer);
+  }
+  
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
         persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case M_TOGGLE_LOGGING:
+      if (record->event.pressed) {
+        log_enable = !log_enable;
       }
       return false;
       break;
--- keyboards/iris/keymaps/custom/rules.mk
+++ keyboards/iris/keymaps/custom/rules.mk
@@ -1,7 +1,6 @@
-RGBLIGHT_ENABLE = yes
-BACKLIGHT_ENABLE = yes
-KEY_LOCK_ENABLE = yes
 TAP_DANCE_ENABLE = yes
+CONSOLE_ENABLE = yes
+KEYLOGGER_ENABLE = yes
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
