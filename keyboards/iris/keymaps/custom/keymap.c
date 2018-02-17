ae21bb327dce2d87468c069361ce886fc6784fde
asdf <koaalex@umich.edu>
2018-02-17 17:30:26 -0500

    Rework NAV and SYM for one handed use.
    Add leader key for easy typing of commonly used scoping characters.
    <>()[]{}

--- keyboards/iris/keymaps/custom/keymap.c
+++ keyboards/iris/keymaps/custom/keymap.c
@@ -23,7 +23,6 @@ enum tap_dance_actions {
   MOD_DANCE = 0
 }; 
 
-#define KC_ KC_TRNS
 #define _______ KC_TRNS
 
 #define WORD_FW LCTL(KC_RIGHT)
@@ -31,16 +30,18 @@ enum tap_dance_actions {
 
 bool log_enable = false;
 
+LEADER_EXTERNS();
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP(
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
      KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5,         KC_6, KC_7, KC_8, KC_9, KC_0, OSL(_LSWAP),
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ENT,
+     KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LEAD,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
      KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,          KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_DEL,     KC_ENT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+     OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_DEL,     KC_ENT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT),
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
                        OSL(_NAV), KC_BSPC, TD(MOD_DANCE),       TD(MOD_DANCE), KC_SPC, OSL(_SYM)
   //                  `----+----+----'        `----+----+----'
@@ -52,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
      _______, KC_Q, KC_W, KC_F, KC_P, KC_G,         KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_ENT,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     KC_TAB, KC_A, KC_R, KC_S, KC_T, KC_D,          KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT,
+     _______, KC_A, KC_R, KC_S, KC_T, KC_D,          KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_DEL,     KC_ENT, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+     _______, KC_Z, KC_X, KC_C, KC_V, KC_B, _______,     _______, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, _______,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                       OSL(_NAV), KC_BSPC, TD(MOD_DANCE),       TD(MOD_DANCE), KC_SPC, OSL(_SYM)
+                       _______, _______, _______,       _______, _______, _______
   //                  `----+----+----'        `----+----+----'
   ),
   
@@ -64,25 +65,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------+--------+--------+--------+--------+--------.                          ,--------+--------+--------+--------+--------+--------.
       _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      KC_CAPS, _______, _______, KC_MINS, KC_UNDS, _______,                            _______, KC_EQL , KC_PLUS, KC_BSLS, _______, KC_SLCK,
+      KC_CAPS, _______, _______, KC_MINS, KC_EQL , KC_PIPE,                            _______, _______, _______, _______, _______, KC_SLCK,
   //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      _______, _______, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT  ,                            KC_GT  , KC_RPRN, KC_RBRC, KC_RCBR, _______, KC_PIPE,
+      _______, _______, _______, KC_UNDS, KC_PLUS, KC_BSLS,                            _______, _______, _______, _______, _______, _______,
   //|--------+--------+--------+--------+--------+--------+--------.        ,--------|--------+--------+--------+--------+--------+--------|
       _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,         KC_F7  ,  KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
   //`--------+--------+--------+----+---+--------+--------+--------/        \--------+--------+--------+---+----+--------+--------+--------'
-                                      _______, _______, OSM(KC_LGUI),        OSM(KC_RGUI), _______, _______
+                                      _______, _______, _______,                _______, _______, _______
   //                                `--------+--------+--------'                `--------+--------+--------'
   ),
   
   [_NAV] = KEYMAP(
     //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-      _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, OSL(_LSWAP), //name?
+      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, //name?
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-      _______, _______, _______, WORD_FW, _______, _______,                            _______, KC_PGUP, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,                            WORD_BW, KC_PGDN, KC_PGUP, WORD_FW, KC_HOME, _______,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-      _______, KC_HOME, _______, KC_PGDN, _______, _______,                            KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_END , _______,
+      _______, _______, _______, _______, _______, _______,                            KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_END , _______,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-      _______, _______, _______, _______, _______, WORD_BW, _______,          _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
                                       _______, _______, _______,                  _______, _______, _______
   //                  `----+----+----'        `----+----+----'
@@ -130,6 +131,48 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
   }
   return true;
+}
+
+
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    leading = false;
+    leader_end();
+
+    SEQ_ONE_KEY(KC_F) {
+      register_code(KC_LSFT);
+      register_code(KC_9);
+      unregister_code(KC_9);
+      register_code(KC_0);
+      unregister_code(KC_0);
+      unregister_code(KC_LSFT);
+    }
+    
+    SEQ_ONE_KEY(KC_D) {
+      register_code(KC_LBRC);
+      unregister_code(KC_LBRC);
+      register_code(KC_RBRC);
+      unregister_code(KC_RBRC);
+    }
+    
+    SEQ_ONE_KEY(KC_G) {
+      register_code(KC_LSFT);
+      register_code(KC_COMM);
+      unregister_code(KC_COMM);
+      register_code(KC_DOT);
+      unregister_code(KC_DOT);
+      unregister_code(KC_LSFT);
+    }
+    
+    SEQ_ONE_KEY(KC_S) {
+      register_code(KC_LSFT);
+      register_code(KC_LBRC);
+      unregister_code(KC_LBRC);
+      register_code(KC_RBRC);
+      unregister_code(KC_RBRC);
+      unregister_code(KC_LSFT);
+    }
+  }
 }
 
 void mod_dance_finished(qk_tap_dance_state_t *state, void *user_data) {
